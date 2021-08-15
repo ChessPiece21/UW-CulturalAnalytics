aacb36d1fed4766397e5567f4ddaf36dff986e23
Joe Lollo <53008442+ChessPiece21@users.noreply.github.com>
2021-08-15 15:54:40 -0700

    Updated stopwords.

--- textbook-work/YellowWallpaper.py
+++ textbook-work/YellowWallpaper.py
@@ -23,8 +23,7 @@ stopwords = ['i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', 'you',
  'in', 'out', 'on', 'off', 'over', 'under', 'again', 'further', 'then', 'once', 'here',
  'there', 'when', 'where', 'why', 'how', 'all', 'any', 'both', 'each', 'few', 'more',
  'most', 'other', 'some', 'such', 'no', 'nor', 'not', 'only', 'own', 'same', 'so',
- 'than', 'too', 'very', 's', 'gutenberg', 'project', 't', 'can', 'will', 'just', 'don', 
-  'should', 'now', 've', 'll', 'amp']
+ 'than', 'too', 'very', 's', 'gutenberg', 'project', 't', 'can', 'will', 'just', 'don', 'should', 'now', 've', 'll', 'amp', 'tm', '1']
 
 # Read in File
 full_text = open(filepath_of_text, encoding="utf-8").read()
